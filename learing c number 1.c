9099c409c7acc5ea07769e52156da4b47c9e830b
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2020-06-22 20:04:35 +0000

    Add files via upload

---
+++ learing c number 1.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+/*Program to print to console*/
+
+main()
+{
+    printf("One small step for a coder. One giant leap for");
+    printf(" programmers.\n");
+    return 0;
+
+}
