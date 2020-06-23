546230b7568980da7fe45f41250bfd88f7005b84
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2020-06-23 09:20:45 +0000

    Add files via upload
    Program that uses the characters, Integers, and Floating Point Data Types

---
+++ learing c number 2- Data Types.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/*Program that uses the characters, Integers, and Floating Point Data Types*/
+
+main()
+{
+    printf("I am learning the %C programming language\n", 'C');
+    printf(" I have just completed Chapter %d\n", 2);
+    printf("I am %.1f percent to move on", 99.9);
+    printf("to the next chapter!\n");
+    return 0;
+
+}
