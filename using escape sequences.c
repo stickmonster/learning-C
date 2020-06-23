df0bc80f5761266ea13af7d264f53aa3845dfc5b
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2020-06-23 22:14:33 +0000

    Add files via upload

---
+++ using escape sequences.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/*Absolute Beginner's Guide to C, 3rd Edition
+Chapter 4 Example 1 -- Chapter4exl.c*/
+
+main()
+{
+
+    //These three lines show how to use popular escape sequences
+    printf("Column A\tColumn B\tColumn C");
+    printf("\nMy Computer\'s Beep sounds like This: \a!\n");
+    printf("\Let'z\bs fix that typo and then show the backslash ");
+    printf("character \\\" she said\n");
+    return 0;
+
+}
